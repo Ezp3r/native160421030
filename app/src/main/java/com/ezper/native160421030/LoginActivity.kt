8979cc2f647e5eff3c574b7da91a744836c98740
GregoriusM <gregoriusmario10@gmail.com>
2024-02-22 14:34:19 +0700

    New Comment Added

--- app/src/main/java/com/ezper/native160421030/LoginActivity.kt
+++ app/src/main/java/com/ezper/native160421030/LoginActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
+
+    ///New Comment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
