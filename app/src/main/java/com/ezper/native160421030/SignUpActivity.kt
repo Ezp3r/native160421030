e2c1b2e8e35bd4b37a624d21d814814232dbef53
Ezper <S160421030@student.ubaya.ac.id>
2024-02-22 14:28:14 +0700

    Revert "sign up activity added"
    This reverts commit 9453deaf4521447448e8b907ebe330080f16c7ed.

--- app/src/main/java/com/ezper/native160421030/SignUpActivity.kt
+++ app/src/main/java/com/ezper/native160421030/SignUpActivity.kt
@@ -5,11 +5,6 @@ import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        /// Sign up fro new user
-        /// 1. cek existing account
-        /// 2. cek password
-        /// 3. implement otp
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
     }
