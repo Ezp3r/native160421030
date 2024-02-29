3bd82e4c2e6ec7399acce10a477d3eae9f359d7d
Ezper <S160421030@student.ubaya.ac.id>
2024-02-29 21:56:31 +0700

    week 2 maybe done

--- app/src/main/java/com/ezper/native160421030/GameFragment.kt
+++ app/src/main/java/com/ezper/native160421030/GameFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.Navigation
 import com.ezper.native160421030.databinding.FragmentGameBinding
 import com.ezper.native160421030.databinding.FragmentMainBinding
+import java.util.Random
 
 class GameFragment : Fragment() {
 
@@ -22,17 +23,34 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rnds1 = (0..100).random()
+        val rnds2 = (0..100).random()
+
+        val score = 0
 
         if(arguments != null){
             val playername = GameFragmentArgs.fromBundle(requireArguments()).playerName
             binding.txtTurn.text = "$playername's turn"
+            binding.txtRand.text = "$rnds1 + $rnds2"
         }
 
-        binding.btnBack.setOnClickListener {
-            val action = GameFragmentDirections.actionMainFragment()
-            Navigation.findNavController(it).navigate(action)
+        binding.btnSubmit.setOnClickListener {
+            val answer = Integer.parseInt(binding.txtAnswer.text.toString())
+            if (rnds1 + rnds2 == answer){
+                score+1
+                val rnds1 = (0..100).random()
+                val rnds2 = (0..100).random()
+                binding.txtRand.text = "$rnds1 + $rnds2"
+                binding.txtAnswer.setText(0)
+            }
+            else{
+                val action = GameFragmentDirections.actionResultFragment(score)
+                Navigation.findNavController(it).navigate(action)
+            }
         }
     }
 
